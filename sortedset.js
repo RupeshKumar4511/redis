a7b1545d537e73dadf2dc367237ae7b123c34811
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-13 18:08:07 +0530

    setsorted updated

---
+++ sortedset.js
@@ -0,0 +1,16 @@
+// Redis sorted set are like priority queue
+import client from "./client.js";
+
+async function init() {
+  await client.zadd(
+    "test_scores",
+    10, "Rakesh",
+    2, "Rajesh",
+    3, "Ramesh"
+  );
+
+  const response = await client.zrange("test_scores", 0, -1, "WITHSCORES");
+  console.log(response);
+}
+
+await init();
