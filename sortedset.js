2dea446276ff1483ac78e565220bb1ebc2014f25
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-14 10:16:36 +0530

    sortedset updated

--- sortedset.js
+++ sortedset.js
@@ -2,15 +2,17 @@
 import client from "./client.js";
 
 async function init() {
-  await client.zadd(
-    "test_scores",
-    10, "Rakesh",
-    2, "Rajesh",
-    3, "Ramesh"
-  );
+  // await client.zadd(
+  //   "test_scores",
+  //   10, "Rakesh",
+  //   2, "Rajesh",
+  //   3, "Ramesh"
+  // );
 
   const response = await client.zrange("test_scores", 0, -1, "WITHSCORES");
-  console.log(response);
+  console.log(response,"\n");
+  const result = await client.zrank("test_scores","Rajesh")
+  console.log(result)
 }
 
 await init();
